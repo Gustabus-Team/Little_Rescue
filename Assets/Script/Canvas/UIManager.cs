80e189651b53285dbff0497a6d6723592a95e0b0
BakerSt21 <ian.gallegos@uniacc.edu>
2021-11-18 11:38:08 -0300

    Ajustando algunas cosas del menu

--- Assets/Script/Canvas/UIManager.cs
+++ Assets/Script/Canvas/UIManager.cs
@@ -15,7 +15,7 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LevelComplete();
+        //LevelComplete();
     }
 
     public void LevelComplete()
