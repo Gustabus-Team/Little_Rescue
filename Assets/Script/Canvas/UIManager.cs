92f842e19854bfc212bb22961baf318d102d76f1
BakerSt21 <ian.gallegos@uniacc.edu>
2021-11-17 21:36:11 -0300

    Update Start Menu and Level 1 Canvas
    Arreglando Canvas del estas 2 escenas

---
+++ Assets/Script/Canvas/UIManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Image levelComplete;
+    public CanvasGroup endLevelPanel, transitionPanel;
+    public GameObject continueButton;
+    public string nextLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelComplete();
+    }
+
+    public void LevelComplete()
+    {
+        StartCoroutine(CompleteAnim());
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine(TransitionLevel());
+    }
+
+    IEnumerator CompleteAnim()
+    {
+        FadePanel(endLevelPanel, 1, 2, true);
+        yield return new WaitForSeconds(2);
+        levelComplete.transform.DOLocalMoveY(0, 2).SetEase(Ease.OutBounce);
+        levelComplete.transform.DOScaleY(1, 2).SetEase(Ease.OutBounce);
+        yield return new WaitForSeconds(2);
+        continueButton.SetActive(true);
+
+        yield break;
+    }
+
+    IEnumerator TransitionLevel()
+    {
+        FadePanel(transitionPanel, 1, 2, true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(nextLevel);
+
+        yield break;
+    }
+
+    public void FadePanel(CanvasGroup panel, int alpha, float time, bool interactive)
+    {
+        panel.DOFade(alpha, time);
+        panel.interactable = interactive;
+        panel.blocksRaycasts = interactive;
+    }
+
+}
